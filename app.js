2c192bee4dfdf5003aa4ba56d71c15fe2b5c3f14
Tejas Pagare <tejaspagare2002@gmail.com>
2020-06-25 19:51:06 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var homeRouter = require('./routes/home');
+var aboutRouter = require('./routes/about');
+var predictRouter = require('./routes/predict');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', homeRouter);
+app.use('/about', aboutRouter);
+app.use('/predict', predictRouter);
+
+
+module.exports = app;
+
+
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000');
